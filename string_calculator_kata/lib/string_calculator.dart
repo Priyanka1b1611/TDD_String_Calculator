b81d46b792c408817b9fe0d639e5f5d42bc6527b
Priyanka1b1611 <priyanka1b1611@gmail.com>
2025-02-07 15:41:14 +0530

    Implementation: Support new line as a delimiter (passes)

--- string_calculator_kata/lib/string_calculator.dart
+++ string_calculator_kata/lib/string_calculator.dart
@@ -1,7 +1,13 @@
 class StringCalculator {
   static int add(String numbers) {
     if (numbers.isEmpty) return 0;
-    var numList = numbers.split(',');
-    return numList.map(int.parse).reduce((a, b) => a + b);
+
+    // Split by comma or newline
+    var numList = numbers.split(RegExp(r'[,\n]'));
+
+    // Filter out empty strings before parsing
+    var parsedNumbers = numList.where((num) => num.isNotEmpty).map(int.parse);
+
+    return parsedNumbers.reduce((a, b) => a + b);
   }
 }
