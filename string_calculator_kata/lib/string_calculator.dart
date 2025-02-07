76ba61be9ed6b6387274a4b55144e53063458896
Priyanka1b1611 <priyanka1b1611@gmail.com>
2025-02-07 15:55:43 +0530

    Implementation: Support custom delimiters (passes)

--- string_calculator_kata/lib/string_calculator.dart
+++ string_calculator_kata/lib/string_calculator.dart
@@ -2,10 +2,17 @@ class StringCalculator {
   static int add(String numbers) {
     if (numbers.isEmpty) return 0;
 
-    // Split by comma or newline
-    var numList = numbers.split(RegExp(r'[,\n]'));
+    RegExp delimiterPattern = RegExp(r'[,\n]'); // Default delimiters: comma or newline
 
-    // Filter out empty strings before parsing
+    // Check for custom delimiter
+    if (numbers.startsWith('//')) {
+      var parts = numbers.split('\n');
+      delimiterPattern = RegExp(RegExp.escape(parts[0].substring(2))); // Extract custom delimiter
+      numbers = parts[1]; // Remove delimiter declaration
+    }
+
+    var numList = numbers.split(delimiterPattern); // Use RegExp to split
+
     var parsedNumbers = numList.where((num) => num.isNotEmpty).map(int.parse);
 
     return parsedNumbers.reduce((a, b) => a + b);
